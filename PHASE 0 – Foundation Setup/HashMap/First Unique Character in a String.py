e9323701a5181be8285edd5254ae4e91f9cdbbd0
M. Nikhil Kumar <123869983+nikhil2213129@users.noreply.github.com>
2025-11-22 22:25:45 +0530

    Implement function to find first unique character

---
+++ PHASE 0 – Foundation Setup/HashMap/First Unique Character in a String.py
@@ -0,0 +1,8 @@
+def firstUniqChar(s):
+    count = {}
+    for char in s:
+        count[char] = count.get(char, 0) + 1
+    for i, char in enumerate(s):
+        if count[char] == 1:
+            return i
+    return -1
