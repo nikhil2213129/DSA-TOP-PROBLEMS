b81e169adf8cd657e3f6047ee4b422f33d113b91
M. Nikhil Kumar <123869983+nikhil2213129@users.noreply.github.com>
2025-11-22 22:28:01 +0530

    Create Count Prime.py

---
+++ PHASE 0 – Foundation Setup/Basic Math/Count Prime.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        class Solution:
+        """
+        arr = [True] * n
+
+        if n == 0 or n == 1:
+            return 0
+
+        arr[0], arr[1] = False, False
+
+        for i in range(2, int(n ** 0.5) + 1):
+            if arr[i]:
+                for j in range(i + i, n, i):
+                    arr[j] = False
+
+        return sum(arr)  
